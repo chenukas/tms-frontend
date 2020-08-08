87b7854838d7756cb7a2ecc25b68d82f67bbda4d
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2020-08-08 19:34:40 +0530

    Created some files

--- src/app/components/dashboard/lecturers/add-lec/add-lec.component.ts
+++ src/app/components/dashboard/lecturers/add-lec/add-lec.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-lec',
   templateUrl: './add-lec.component.html',
   styleUrls: ['./add-lec.component.scss']
 })
+
+@NgModule({
+  imports: [
+    FormControl,
+    FormBuilder,
+    FormGroup,
+    Validators,
+    ReactiveFormsModule
+  ],
+})
+
 export class AddLecComponent implements OnInit {
 
   constructor() { }
