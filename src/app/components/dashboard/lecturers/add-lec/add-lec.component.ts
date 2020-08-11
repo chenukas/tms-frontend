c15187aac332538877e89fc35c40dc849d2655da
Chenuka Sumanasekara <58046625+chenukas@users.noreply.github.com>
2020-08-12 03:54:25 +0530

    Fixed issues

--- src/app/components/dashboard/lecturers/add-lec/add-lec.component.ts
+++ src/app/components/dashboard/lecturers/add-lec/add-lec.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, NgModule } from '@angular/core';
-import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatButtonModule } from '@angular/material/button';
+//import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+//import { MatFormFieldModule } from '@angular/material/form-field';
+//import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-add-lec',
@@ -9,16 +9,16 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrls: ['./add-lec.component.scss']
 })
 
-@NgModule({
-  imports: [
-    FormControl,
-    FormBuilder,
-    FormGroup,
-    Validators,
-    MatFormFieldModule,
-    MatButtonModule
-  ],
-})
+//@NgModule({
+//  imports: [
+//    FormControl,
+//    FormBuilder,
+//    FormGroup,
+//    Validators,
+//    MatFormFieldModule,
+//    MatButtonModule
+//  ],
+//})
 
 export class AddLecComponent implements OnInit {
 
