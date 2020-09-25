e852b5aceeae689caaa4cc31d3f579b480c73d57
unknown <chenukasumanasekara@gmail.com>
2020-09-25 15:57:49 +0530

    Completed marking non-overlapping subjects

--- src/app/components/dashboard/manager/non-overlapping-s/non-overlapping-s.component.ts
+++ src/app/components/dashboard/manager/non-overlapping-s/non-overlapping-s.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { SubjectsService } from 'app/services/subjects.service';
+import { MatTableDataSource } from '@angular/material/table';
+
+interface APIResponse {
+  success : boolean,
+  data : any
+}
 
 @Component({
   selector: 'app-non-overlapping-s',
@@ -7,9 +14,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NonOverlappingSComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns = ['subCode', 'subName', 'action'];
+  dataSource : MatTableDataSource< any >;
+  public noolapping: Boolean;
+
+  constructor(
+    private subjectsService: SubjectsService
+  ) { }
 
   ngOnInit(): void {
+    this.noolapping  = false;
+    this.viewAllNonParallelSubjects();
+  }
+
+  viewAllNonParallelSubjects() {
+    this.subjectsService.viewNonParallelSubjects().subscribe((res: APIResponse) => {
+      this.dataSource = new MatTableDataSource ( res.data ); 
+    });
+  }
+
+  updateSubjectNoolapping(subject: any) {
+    this.noolapping = true;
+    this.subjectsService.updateSubjectNoolappingById(subject._id,this.noolapping).subscribe(response => {
+      console.log(response);
+      this.viewAllNonParallelSubjects();
+      
+    }, err => {
+      console.log(err.message);
+    });
+
   }
 
 }
