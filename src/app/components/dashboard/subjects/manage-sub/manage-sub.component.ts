54a6e218a7307efd06d611d75b77625038af735c
Chamodi Thennakoon <58078417+chamodit@users.noreply.github.com>
2020-10-01 21:37:47 +0530

    Done some changes in html files

--- src/app/components/dashboard/subjects/manage-sub/manage-sub.component.ts
+++ src/app/components/dashboard/subjects/manage-sub/manage-sub.component.ts
@@ -1,25 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { SubjectsService } from 'app/services/subjects.service';
-import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, OnInit } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { SubjectsService } from "app/services/subjects.service";
+import { Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 interface APIResponse {
-  success : boolean,
-  data : any
+  success: boolean;
+  data: any;
 }
 
 @Component({
-  selector: 'app-manage-sub',
-  templateUrl: './manage-sub.component.html',
-  styleUrls: ['./manage-sub.component.scss']
+  selector: "app-manage-sub",
+  templateUrl: "./manage-sub.component.html",
+  styleUrls: ["./manage-sub.component.scss"],
 })
 export class ManageSubComponent implements OnInit {
+  displayedColumns = ["subCode", "subName", "action"];
+  dataSource: MatTableDataSource<any>;
 
-  displayedColumns = ['subCode', 'subName', 'action'];
-  dataSource : MatTableDataSource< any >;
-
-  private _id : string;
+  private _id: string;
   private year: string;
   private semester: string;
   private name: string;
@@ -35,34 +34,40 @@ export class ManageSubComponent implements OnInit {
     private subjectsService: SubjectsService,
     private router: Router,
     private snackBar: MatSnackBar
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this._id = '';
-    this.name = '';
-    this.code = '';
+    this._id = "";
+    this.name = "";
+    this.code = "";
     this.viewAllSubjects();
   }
 
   viewAllSubjects() {
     this.subjectsService.viewSubjects().subscribe((res: APIResponse) => {
-      this.dataSource = new MatTableDataSource ( res.data ); 
-    })
+      this.dataSource = new MatTableDataSource(res.data);
+    });
   }
 
   updateSubject(id: String) {
-    this.router.navigate(['/subjects/add'], {queryParams: {id} });
+    this.router.navigate(["/subjects/add"], { queryParams: { id } });
   }
 
-  deleteSubject(id: String){
-    this.subjectsService.deleteSubjectById(id).subscribe(response => {
-      console.log(response);
-      this.snackBar.open('Details are successfully deleted', null, { duration : 2000});
-      this.viewAllSubjects();
-      
-    }, err => {
-      this.snackBar.open('Details could not be deleted', null, { duration : 3000});
-      console.log(err.message);
-    });
+  deleteSubject(id: String) {
+    this.subjectsService.deleteSubjectById(id).subscribe(
+      (response) => {
+        console.log(response);
+        this.snackBar.open("Details are successfully deleted", null, {
+          duration: 2000,
+        });
+        this.viewAllSubjects();
+      },
+      (err) => {
+        this.snackBar.open("Details could not be deleted", null, {
+          duration: 3000,
+        });
+        console.log(err.message);
+      }
+    );
   }
 }
